e887a338c90fba3155687a8c9d0417c6080f6145
wataru0531 <obito0531@gmail.com>
2024-03-10 00:08:41 +0900

    next.config.jsを修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // 静的エクスポートを有効にする
-
+  output: 'export', // 静的エクスポートを有効にする
+  distDir: 'dist',
 };
 
 export default nextConfig;
